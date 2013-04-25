dad6b6ee28eac7c98b93389cf5f31ecb562e1090
Joshua Merrell <joshuamerrell@gmail.com>
2013-04-25 12:14:27 -0700

    added settings menu

--- html/javascript/gochan.js
+++ html/javascript/gochan.js
@@ -1,5 +1,52 @@
 var $jq = jQuery.noConflict();
 
+var down_arrow_symbol = "&#9660;";
+var up_arrow_symbol = "&#9650;";
+var topbar;
+var setings_menu;
+var settings_arr = [];
+
+var SettingsMenuOption = {
+	"name":"",
+	"cookieName":"",
+	"cookieValue":"",
+	"cookieType":"",
+}
+
+var DropDownMenu = function(title,callback) {
+	this.callback = callback;
+	this.buttonTitle = title;
+	$jq("div#topbar").append("<ul><a href=\"#\" class=\"dropdown-button\" id=\""+title.toLowerCase()+"\"><li>"+title+down_arrow_symbol+"</li></a></ul>");
+	this.button_jq = $jq("div#topbar a#"+title.toLowerCase());
+	this.button_jq.click(function(){
+		//$jq(document.body).append("<div id="+title.toLowerCase())
+		callback();
+	})
+}
+
+DropDownMenu.prototype.open = function() {
+
+}
+
+DropDownMenu.prototype.close = function() {
+
+}
+
+DropDownMenu.prototype.isOpen = function() {
+
+}
+
+function showLightBox(title,innerHTML) {
+	$jq(document.body).prepend("<div class=\"lightbox-bg\"></div><div class=\"lightbox\"><div class=\"lightbox-title\">"+title+"<a href=\"#\" class=\"lightbox-x\">X</a><hr /></div>"+innerHTML+"</div>");
+	$jq("a.lightbox-x").click(function() {
+		$jq(".lightbox").remove();
+		$jq(".lightbox-bg").remove();
+	});
+}
+
+function generateSettingsList() {
+
+}
 
 function changeFrontPage(page_name) {
 	var tabs = $jq(".tab");
@@ -101,6 +148,17 @@ function isThreadPage() {
 }
 
 $jq(document).ready(function() {
+	topbar = $jq("div#topbar");
+	var settings_html = "<table width=\"100%\"><colgroup><col span=\"1\" width=\"50%\"><col span=\"1\" width=\"50%\"></colgroup><tr><td><b>Style:</b></td><td><select name=\"style\" style=\"min-width:50%\">"
+	for(var i = 0; i < styles.length; i++) {
+		settings_html += "<option value=\""+styles[i]+"\">"+styles[i][0].toUpperCase()+styles[i].substring(1,styles[i].length);
+	}
+	settings_html+="</select></td><tr><tr><td><b>Pin top bar:</b></td><td><input type=\"checkbox\" /></td></tr></table><div class=\"lightbox-footer\"><hr /><button id=\"save-settings-button\">Save Settings</button></div>"
+
+ 	setings_menu = new DropDownMenu("Settings",function(){
+ 		showLightBox("Settings",settings_html)
+ 	});
+ 	b = new DropDownMenu("WT",function() {});
 	if(isFrontPage()) {
 		changeFrontPage(getHashVal());
 	}
@@ -114,5 +172,6 @@ $jq(document).ready(function() {
 			block.hide();
 			$jq(this).html("+");
 		}
+
 	});
 });
