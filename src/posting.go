6ecdc3b5fedc712aa070fcfb84194381759d9cf0
Joshua Merrell <rodaz143@gmail.com>
2013-10-02 20:19:39 -0700

    don't remember the email if it's 'sage'

--- src/posting.go
+++ src/posting.go
@@ -418,7 +418,10 @@ func makePost(w http.ResponseWriter, r *http.Request) {
 	post.Message = escapeString(strings.Replace(html.EscapeString(request.FormValue("postmsg")), "\n", "<br />", -1))
 	post.Password = md5_sum(request.FormValue("postpassword"))
 	http.SetCookie(writer, &http.Cookie{Name: "name", Value: post.Name, Path: "/", Domain: config.SiteDomain, RawExpires: getSpecificSQLDateTime(time.Now().Add(time.Duration(31536000))),MaxAge: 31536000})
-	http.SetCookie(writer, &http.Cookie{Name: "email", Value: post.Email, Path: "/", Domain: config.SiteDomain, RawExpires: getSpecificSQLDateTime(time.Now().Add(time.Duration(31536000))),MaxAge: 31536000})
+	if post_email != "sage" {
+		http.SetCookie(writer, &http.Cookie{Name: "email", Value: post.Email, Path: "/", Domain: config.SiteDomain, RawExpires: getSpecificSQLDateTime(time.Now().Add(time.Duration(31536000))),MaxAge: 31536000})
+	}
+	
 	http.SetCookie(writer, &http.Cookie{Name: "password", Value: request.FormValue("postpassword"), Path: "/", Domain: config.SiteDomain, RawExpires: getSpecificSQLDateTime(time.Now().Add(time.Duration(31536000))),MaxAge: 31536000})	
 	post.IP = request.RemoteAddr
 	post.Timestamp = time.Now()
