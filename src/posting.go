ced82c6a268df2094bb4662829d81b4d4878d506
Joshua Merrell <joshuamerrell@gmail.com>
2014-04-04 12:54:35 -0700

    don't truncate the post IP after a colon because that messes up IPv6

--- src/posting.go
+++ src/posting.go
@@ -488,7 +488,7 @@ func makePost(w http.ResponseWriter, r *http.Request) {
 	http.SetCookie(writer, &http.Cookie{Name: "password", Value: request.FormValue("postpassword"), Path: "/", Domain: config.SiteDomain, RawExpires: getSpecificSQLDateTime(time.Now().Add(time.Duration(31536000))),MaxAge: 31536000})	
 	//http.SetCookie(writer, &http.Cookie{Name: "password", Value: request.FormValue("postpassword"), Path: "/", Domain: config.Domain, RawExpires: getSpecificSQLDateTime(time.Now().Add(time.Duration(31536000))),MaxAge: 31536000})
 
-	post.IP = request.RemoteAddr[:strings.Index(request.RemoteAddr,":")]
+	post.IP = request.RemoteAddr
 	post.Timestamp = time.Now()
 	post.PosterAuthority = getStaffRank()
 	post.Bumped = time.Now()
