a270ede3ca53fa35481dc625823cb24c08b1ab47
Joshua Merrell <joshuamerrell@gmail.com>
2015-01-09 22:54:15 -0800

    improve build script to add (some) support for Windows and FreeBSD stuff

--- build.sh
+++ build.sh
@@ -1,3 +1,18 @@
-#!/bin/bash
-export GOPATH=$PWD/lib
-go build -v -o gochan ./src
+#!/usr/bin/env bash
+if [ -z $GOPATH ]
+then
+	#export GOPATH=$PWD/lib
+	echo "\$GOPATH not set. Please run 'export GOPATH=\$PWD/lib' (or wherever you prefer) and run this again."
+	exit
+fi
+CGO_ENABLED=0
+GOARCH=amd64
+GOOS=windows
+SUFFIX=""
+if [ $GOOS == "windows" ]
+then
+	SUFFIX=".exe"
+fi
+
+go build -v  -ldflags "-w" -o gochan$SUFFIX ./src
+# the -w ldflag omits debugging stuff
