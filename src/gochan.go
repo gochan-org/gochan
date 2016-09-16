76cb0ecafa78511becfe9ad8ca9158aa2e632214
Joshua Merrell <joshuamerrell@gmail.com>
2016-09-16 00:44:01 -0700

    fix verbsotity variable name for last commit

--- src/gochan.go
+++ src/gochan.go
@@ -11,13 +11,13 @@ var version string
 // verbose = 1 for non-critical warnings and important info
 // verbose = 2 for all debugging/benchmarks/warnings
 // set in build.sh via -ldflags
-var verbose_str string
+var verbosity_str string
 var buildtime_str string // set in build.sh, format: YRMMDD.HHMM
 
 func main() {
 	defer db.Close()
 	initConfig()
-	config.Verbosity, _ = strconv.Atoi(verbose_str)
+	config.Verbosity, _ = strconv.Atoi(verbosity_str)
 	config.Version = version
 	printf(0, "Starting gochan v%s.%s, using verbosity level %d\n", config.Version, buildtime_str, config.Verbosity)
 	printf(0, "Config file loaded. Connecting to database...")
