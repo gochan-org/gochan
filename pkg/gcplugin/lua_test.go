7e87a9ec10173cafcb92151dac7c2d96696f1c72
Eggbertx <eggbertx@users.noreply.github.com>
2022-06-01 15:05:26 -0700

    Test value modification with gcplugin.TestStructPassing

--- pkg/gcplugin/lua_test.go
+++ pkg/gcplugin/lua_test.go
@@ -36,8 +36,14 @@ func TestStructPassing(t *testing.T) {
 		MessageText: "Message text\n",
 	}
 	lState.SetGlobal("post", luar.New(lState, p))
-	err := lState.DoString(`print(string.format("Receiving post from %q", post.Name))`)
+	err := lState.DoString(`print(string.format("Receiving post from %q", post.Name))
+print(string.format("Message before changing: %q", post.MessageText))
+post.MessageText = "Modified message text\n"
+post.MessageHTML = "Modified message HTML<br />"
+print(string.format("Modified message text: %q", post.MessageText))
+`)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
+	t.Logf("Modified message text after Lua: %q", p.MessageText)
 }
