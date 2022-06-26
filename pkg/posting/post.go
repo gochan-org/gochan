gochan-org/gochan

commit 0415b3066add0b360a669d0ec7ad7921f49106d0

If upload is gif and not bigger than a thumbnail, still create a thumbnail if it's animated

pkg/posting/post.go
@@ -342,7 +342,22 @@ func MakePost(writer http.ResponseWriter, request *http.Request) {
 					serverutil.ServeErrorPage(writer, err.Error())
 					return
 				}
-			} else if boardConfig.ThumbWidth >= post.ImageW && boardConfig.ThumbHeight >= post.ImageH {
+			}
+			shouldThumb := false
+			if boardConfig.ThumbWidth >= post.ImageW && boardConfig.ThumbHeight >= post.ImageH {
+				if ext == "gif" {
+					// upload is a GIF, check if it's animated and thumbnail it if it is
+					numFrames, err := numImageFrames(filePath)
+					if err != nil {
+						serverutil.ServeErrorPage(writer, err.Error())
+						return
+					}
+					if numFrames > 1 {
+						shouldThumb = true
+					}
+				}
+			}
+			if !shouldThumb {
 				// If image fits in thumbnail size, symlink thumbnail to original
 				post.ThumbW = img.Bounds().Max.X
 				post.ThumbH = img.Bounds().Max.Y

pkg/posting/uploads.go
@@ -3,7 +3,9 @@ package posting
 import (
 	"errors"
 	"image"
+	"image/gif"
 	"math/rand"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
@@ -11,6 +13,7 @@ import (
 
 	"github.com/disintegration/imaging"
 	"github.com/gochan-org/gochan/pkg/config"
+	"github.com/gochan-org/gochan/pkg/gcutil"
 )
 
 func createImageThumbnail(imageObj image.Image, size string) image.Image {
@@ -109,4 +112,21 @@ func getThumbnailSize(w, h int, size string) (newWidth, newHeight int) {
 		newHeight = int(float32(h) * percent)
 	}
 	return
+}
+
+func numImageFrames(imgPath string) (int, error) {
+	ext := strings.ToLower(gcutil.GetFileExtension(imgPath))
+	if ext != "gif" {
+		return 1, nil
+	}
+	fi, err := os.Open(imgPath)
+	if err != nil {
+		return 0, err
+	}
+	defer fi.Close()
+	g, err := gif.DecodeAll(fi)
+	if err != nil {
+		return 0, err
+	}
+	return len(g.Image), nil
 }

pkg/gcsql/tables.go
@@ -278,6 +278,20 @@ type Post struct {
 	Stickied         bool          `json:"-"`
 	Locked           bool          `json:"-"`
 	Reviewed         bool          `json:"-"`
+
+	boardDir string
+}
+
+func (p *Post) GetBoardDir() (string, error) {
+	if p.boardDir != "" {
+		return p.boardDir, nil
+	}
+	const sql = `SELECT dir FROM DBPREFIXboards WHERE id = (
+		SELECT board_id FROM DBPREFIXthreads WHERE id = (
+			SELECT thread_id FROM DBPREFIXposts WHERE id = ? LIMIT 1) LIMIT 1)`
+
+	err := QueryRowSQL(sql, []interface{}{p.ParentID}, interfaceSlice(&p.boardDir))
+	return p.boardDir, err
 }
 
 func (p *Post) GetURL(includeDomain bool) string {

pkg/gcutil/util.go
@@ -25,7 +25,7 @@ import (
 )
 
 const (
-	// DefaultMaxAge is used for cookies that have an invalid or unset max age (default is 1 year)
+	// DefaultMaxAge is used for cookies that have an invalid or unset max age (default is 1 month)
 	DefaultMaxAge = 60 * 60 * 24 * 31
 )
 

commit 9d0c8d414926fa24aa131c757727ac0f7decee5e

Fix bug with thumbnails not being created even if image is bigger than a thumbnail (bug introduced in commit 0415b3066add0b360a669d0ec7ad7921f49106d0)

pkg/posting/post.go
@@ -37,7 +37,6 @@ const (
 func MakePost(writer http.ResponseWriter, request *http.Request) {
 	var maxMessageLength int
 	var post gcsql.Post
-	// domain := request.Host
 	var formName string
 	var nameCookie string
 	var formEmail string
@@ -49,8 +48,6 @@ func MakePost(writer http.ResponseWriter, request *http.Request) {
 		http.Redirect(writer, request, systemCritical.WebRoot, http.StatusFound)
 		return
 	}
-	// fix new cookie domain for when you use a port number
-	// domain = chopPortNumRegex.Split(domain, -1)[0]
 
 	post.ParentID, _ = strconv.Atoi(request.FormValue("threadid"))
 	post.BoardID, _ = strconv.Atoi(request.FormValue("boardid"))
@@ -296,11 +293,11 @@ func MakePost(writer http.ResponseWriter, request *http.Request) {
 						post.Filesize = value
 					}
 				}
+				thumbType := "reply"
 				if post.ParentID == 0 {
-					post.ThumbW, post.ThumbH = getThumbnailSize(post.ImageW, post.ImageH, "op")
-				} else {
-					post.ThumbW, post.ThumbH = getThumbnailSize(post.ImageW, post.ImageH, "reply")
+					thumbType = "op"
 				}
+				post.ThumbW, post.ThumbH = getThumbnailSize(post.ImageW, post.ImageH, boardDir, thumbType)
 			}
 		} else {
 			// Attempt to load uploaded file with imaging library
@@ -323,11 +320,11 @@ func MakePost(writer http.ResponseWriter, request *http.Request) {
 			// Get image width and height, as well as thumbnail width and height
 			post.ImageW = img.Bounds().Max.X
 			post.ImageH = img.Bounds().Max.Y
+			thumbType := "reply"
 			if post.ParentID == 0 {
-				post.ThumbW, post.ThumbH = getThumbnailSize(post.ImageW, post.ImageH, "op")
-			} else {
-				post.ThumbW, post.ThumbH = getThumbnailSize(post.ImageW, post.ImageH, "reply")
+				thumbType = "op"
 			}
+			post.ThumbW, post.ThumbH = getThumbnailSize(post.ImageW, post.ImageH, boardDir, thumbType)
 
 			gclog.Printf(gclog.LAccessLog, "Receiving post with image: %q from %s, referrer: %s",
 				handler.Filename, post.IP, request.Referer())
@@ -335,7 +332,7 @@ func MakePost(writer http.ResponseWriter, request *http.Request) {
 			if request.FormValue("spoiler") == "on" {
 				// If spoiler is enabled, symlink thumbnail to spoiler image
 				if _, err := os.Stat(path.Join(systemCritical.DocumentRoot, "spoiler.png")); err != nil {
-					serverutil.ServeErrorPage(writer, "missing /spoiler.png")
+					serverutil.ServeErrorPage(writer, "missing spoiler.png")
 					return
 				}
 				if err = syscall.Symlink(path.Join(systemCritical.DocumentRoot, "spoiler.png"), thumbPath); err != nil {
@@ -343,47 +340,44 @@ func MakePost(writer http.ResponseWriter, request *http.Request) {
 					return
 				}
 			}
-			shouldThumb := false
-			if boardConfig.ThumbWidth >= post.ImageW && boardConfig.ThumbHeight >= post.ImageH {
-				if ext == "gif" {
-					// upload is a GIF, check if it's animated and thumbnail it if it is
-					numFrames, err := numImageFrames(filePath)
-					if err != nil {
-						serverutil.ServeErrorPage(writer, err.Error())
-						return
-					}
-					if numFrames > 1 {
-						shouldThumb = true
-					}
-				}
-			}
-			if !shouldThumb {
-				// If image fits in thumbnail size, symlink thumbnail to original
-				post.ThumbW = img.Bounds().Max.X
-				post.ThumbH = img.Bounds().Max.Y
-				if err := syscall.Symlink(filePath, thumbPath); err != nil {
-					serverutil.ServeErrorPage(writer, err.Error())
-					return
-				}
-			} else {
+
+			shouldThumb := shouldCreateThumbnail(filePath, post.ImageW, post.ImageH, post.ThumbW, post.ThumbH)
+			if shouldThumb {
 				var thumbnail image.Image
 				var catalogThumbnail image.Image
 				if post.ParentID == 0 {
 					// If this is a new thread, generate thumbnail and catalog thumbnail
-					thumbnail = createImageThumbnail(img, "op")
-					catalogThumbnail = createImageThumbnail(img, "catalog")
+					thumbnail = createImageThumbnail(img, boardDir, "op")
+					catalogThumbnail = createImageThumbnail(img, boardDir, "catalog")
 					if err = imaging.Save(catalogThumbnail, catalogThumbPath); err != nil {
 						serverutil.ServeErrorPage(writer, gclog.Print(gclog.LErrorLog,
 							"Couldn't generate catalog thumbnail: ", err.Error()))
 						return
 					}
 				} else {
-					thumbnail = createImageThumbnail(img, "reply")
+					thumbnail = createImageThumbnail(img, boardDir, "reply")
 				}
 				if err = imaging.Save(thumbnail, thumbPath); err != nil {
 					serverutil.ServeErrorPage(writer, gclog.Print(gclog.LErrorLog,
 						"Couldn't save thumbnail: ", err.Error()))
 					return
+				}
+			} else {
+				// If image fits in thumbnail size, symlink thumbnail to original
+				post.ThumbW = img.Bounds().Max.X
+				post.ThumbH = img.Bounds().Max.Y
+				if err := syscall.Symlink(filePath, thumbPath); err != nil {
+					serverutil.ServeErrorPage(writer, "Couldn't create thumbnail: "+err.Error())
+					return
+				}
+				if post.ParentID == 0 {
+					// Generate catalog thumbnail
+					catalogThumbnail := createImageThumbnail(img, boardDir, "catalog")
+					if err = imaging.Save(catalogThumbnail, catalogThumbPath); err != nil {
+						serverutil.ServeErrorPage(writer, gclog.Print(gclog.LErrorLog,
+							"Couldn't generate catalog thumbnail: ", err.Error()))
+						return
+					}
 				}
 			}
 		}

pkg/posting/uploads.go
@@ -7,39 +7,58 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
+	"path"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/disintegration/imaging"
 	"github.com/gochan-org/gochan/pkg/config"
+	"github.com/gochan-org/gochan/pkg/gclog"
 	"github.com/gochan-org/gochan/pkg/gcutil"
 )
 
-func createImageThumbnail(imageObj image.Image, size string) image.Image {
-	var thumbWidth int
-	var thumbHeight int
-	boardCfg := config.GetBoardConfig("")
-
-	switch size {
+func getBoardThumbnailSize(boardDir string, thumbType string) (int, int) {
+	boardCfg := config.GetBoardConfig(boardDir)
+	switch thumbType {
 	case "op":
-		thumbWidth = boardCfg.ThumbWidth
-		thumbHeight = boardCfg.ThumbHeight
+		return boardCfg.ThumbWidth, boardCfg.ThumbHeight
 	case "reply":
-		thumbWidth = boardCfg.ThumbWidthReply
-		thumbHeight = boardCfg.ThumbHeightReply
+		return boardCfg.ThumbWidthReply, boardCfg.ThumbHeightReply
 	case "catalog":
-		thumbWidth = boardCfg.ThumbWidthCatalog
-		thumbHeight = boardCfg.ThumbHeightCatalog
+		return boardCfg.ThumbWidth, boardCfg.ThumbHeight
 	}
+	// todo: use reflect package to print location to error log, because this shouldn't happen
+	return -1, -1
+}
+
+func createImageThumbnail(imageObj image.Image, boardDir string, thumbType string) image.Image {
+	thumbWidth, thumbHeight := getBoardThumbnailSize(boardDir, thumbType)
+
 	oldRect := imageObj.Bounds()
 	if thumbWidth >= oldRect.Max.X && thumbHeight >= oldRect.Max.Y {
 		return imageObj
 	}
 
-	thumbW, thumbH := getThumbnailSize(oldRect.Max.X, oldRect.Max.Y, size)
+	thumbW, thumbH := getThumbnailSize(oldRect.Max.X, oldRect.Max.Y, boardDir, thumbType)
 	imageObj = imaging.Resize(imageObj, thumbW, thumbH, imaging.CatmullRom) // resize to 600x400 px using CatmullRom cubic filter
 	return imageObj
+}
+
+func shouldCreateThumbnail(imgPath string, imgWidth int, imgHeight int, thumbWidth int, thumbHeight int) bool {
+	ext := strings.ToLower(path.Ext(imgPath))
+	if ext == ".gif" {
+		numFrames, err := numImageFrames(imgPath)
+		if err != nil {
+			gclog.Printf(gclog.LErrorLog, "Error processing %q: %s", imgPath, err.Error())
+			return true
+		}
+		if numFrames > 1 {
+			return true
+		}
+	}
+
+	return imgWidth > thumbWidth || imgHeight > thumbHeight
 }
 
 func createVideoThumbnail(video, thumb string, size int) error {
@@ -83,33 +102,23 @@ func getNewFilename() string {
 }
 
 // find out what out thumbnail's width and height should be, partially ripped from Kusaba X
-func getThumbnailSize(w, h int, size string) (newWidth, newHeight int) {
-	var thumbWidth int
-	var thumbHeight int
-	boardCfg := config.GetBoardConfig("")
-	switch {
-	case size == "op":
-		thumbWidth = boardCfg.ThumbWidth
-		thumbHeight = boardCfg.ThumbHeight
-	case size == "reply":
-		thumbWidth = boardCfg.ThumbWidthReply
-		thumbHeight = boardCfg.ThumbHeightReply
-	case size == "catalog":
-		thumbWidth = boardCfg.ThumbWidthCatalog
-		thumbHeight = boardCfg.ThumbHeightCatalog
-	}
-	if w == h {
+func getThumbnailSize(uploadWidth, uploadHeight int, boardDir string, thumbType string) (newWidth, newHeight int) {
+	thumbWidth, thumbHeight := getBoardThumbnailSize(boardDir, thumbType)
+	if uploadWidth < thumbWidth && uploadHeight < thumbHeight {
+		newWidth = uploadWidth
+		newHeight = uploadHeight
+	} else if uploadWidth == uploadHeight {
 		newWidth = thumbWidth
 		newHeight = thumbHeight
 	} else {
 		var percent float32
-		if w > h {
-			percent = float32(thumbWidth) / float32(w)
+		if uploadWidth > uploadHeight {
+			percent = float32(thumbWidth) / float32(uploadWidth)
 		} else {
-			percent = float32(thumbHeight) / float32(h)
+			percent = float32(thumbHeight) / float32(uploadHeight)
 		}
-		newWidth = int(float32(w) * percent)
-		newHeight = int(float32(h) * percent)
+		newWidth = int(float32(uploadWidth) * percent)
+		newHeight = int(float32(uploadHeight) * percent)
 	}
 	return
 }