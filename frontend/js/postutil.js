d0a889b3696f4385d34a1929b95ccd4711c7606a
Eggbertx <eggbertx@users.noreply.github.com>
2022-08-04 16:19:18 -0700

    Format filesize of incoming posts in current thread

--- frontend/js/postutil.js
+++ frontend/js/postutil.js
@@ -87,11 +87,26 @@ export function insideOP(elem) {
 
 /**
  * Formats the timestamp strings from JSON into a more readable format
- * @param 
+ * @param {string} dateStr timestamp string, assumed to be in ISO Date-Time format
  */
 function formatDateString(dateStr) {
 	let date = new Date(dateStr);
 	return date.toDateString() + ", " + date.toLocaleTimeString();
+}
+
+/**
+ * Formats the given number of bytes into an easier to read filesize
+ * @param {number} size
+ */
+function formatFileSize(size) {
+	if(size < 1000) {
+		return `${size} B`;
+	} else if(size <= 100000) {
+		return `${(size/1024).toFixed(1)} KB`;
+	} else if(size <= 100000000) {
+		return `${(size/1024/1024).toFixed(2)} MB`;
+	}
+	return `${(size/1024/1024/1024).toFixed(2)} GB`;
 }
 
 /**
@@ -156,7 +171,7 @@ function createPostElement(post, boardDir, elementClass = "inlinepostprev") {
 						href: webroot + boardDir + "/src/" + post.tim,
 						target: "_blank"
 					}).text(post.tim),
-					` - (## MB , ${post.w}x${post.h},`,
+					` - (${formatFileSize(post.fsize)} , ${post.w}x${post.h}, `,
 					$("<a/>").prop({
 						class: "file-orig",
 						href: webroot + boardDir + "/src/" + post.tim,
