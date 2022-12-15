fd97da86cd7f00778b406133e2d5b646493eaf88
Eggbertx <eggbertx@users.noreply.github.com>
2022-12-15 15:19:15 -0800

    Add sqlite3 support to vagrant tools

--- devtools/dbconnect.sh
+++ devtools/dbconnect.sh
@@ -6,6 +6,8 @@ if [ "$DBTYPE" = "mysql" ] || [ -z "$DBTYPE" ]; then
 	mysql -stu gochan -D gochan -pgochan
 elif [ "$DBTYPE" = "postgresql" ]; then
 	psql -U gochan -h 127.0.0.1 gochan
+elif [ "$DBTYPE" = "sqlite3" ]; then
+	sqlite3 /etc/gochan/gochan.db
 else
 	echo "DB type '$DBTYPE' not supported"
 fi
