ae632c5402ff39d7cc76284fd022a3ee27704040
Eggbertx <eggbertx@users.noreply.github.com>
2022-12-17 11:48:15 -0800

    Make Python changes suggested by DeepSource

--- devtools/selenium_testing/runtests.py
+++ devtools/selenium_testing/runtests.py
@@ -14,8 +14,8 @@ from urllib.parse import urljoin
 from selenium import webdriver
 
 from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.edge.webdriver import WebDriver
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.edge.webdriver import WebDriver # skipcq:  PYL-W0404
+from selenium.webdriver.firefox.webdriver import WebDriver # skipcq:  PYL-W0404
 from selenium.webdriver.chrome.options import Options  as ChromeOptions
 from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
@@ -36,7 +36,7 @@ testingUploadPath = "../../html/notbanned.png"
 testingPassword = "12345"
 testingBoard = "test"
 
-threadRE = re.compile('.*/(\S+)/(\d+)(\+50)?.html')
+threadRE = re.compile(r'.*/(\S+)/(\d+)(\+50)?.html')
 boardTitleRE = re.compile('/(\w+)/ - (.+)')
 browser = ""
 headless = False
@@ -50,12 +50,15 @@ def boardExists(board:str):
 			return True
 	return False
 
+
 def gotoPage(driver: WebDriver, page: str):
 	driver.get(urljoin(testingSite, page))
+
 
 def isLoggedIn(driver: WebDriver):
 	gotoPage(driver, "manage?action=login")
 	return driver.find_element(by=By.CSS_SELECTOR, value="h1#board-title").text == "Dashboard"
+
 
 def loginToStaff(driver: WebDriver, username = "admin", pw = "password"):
 	if isLoggedIn(driver):
@@ -64,6 +67,7 @@ def loginToStaff(driver: WebDriver, username = "admin", pw = "password"):
 	driver.find_element(by=By.NAME, value="username").send_keys(username)
 	driver.find_element(by=By.NAME, value="password").send_keys(pw)
 	driver.find_element(by=By.CSS_SELECTOR, value="input[value=Login]").click()
+
 
 def makePostOnPage(url: str, runner: unittest.TestCase):
 	gotoPage(runner.driver, url)
@@ -81,7 +85,8 @@ def makePostOnPage(url: str, runner: unittest.TestCase):
 		path.abspath(testingUploadPath),
 		testingPassword)
 	WebDriverWait(runner.driver, 10).until(
-		EC.url_matches(threadRE))	
+		EC.url_matches(threadRE))
+
 
 def sendPost(postForm:WebElement, name="", email="", subject="", message="", file="", password=""):
 	postForm.find_element(by=By.NAME, value="postname").send_keys(name)
@@ -96,20 +101,25 @@ def sendPost(postForm:WebElement, name="", email="", subject="", message="", fil
 		passwordInput.send_keys(password)
 	postForm.find_element(by=By.CSS_SELECTOR, value="input[type=submit]").click()
 
+
 def deletePost(driver:WebDriver, postID:int, password:str):
 	driver.find_element(by=By.CSS_SELECTOR, value=("input#check%s"%postID)).click()
 	if password != "":
-		delPasswordInput = driver.find_element(by=By.CSS_SELECTOR, value="input#delete-password")
+		delPasswordInput = driver.find_element(
+			by=By.CSS_SELECTOR,
+			value="input#delete-password")
 		delPasswordInput.clear()
 		delPasswordInput.send_keys(password)
-	driver.find_element(by=By.CSS_SELECTOR, value="input[name=delete_btn]").click()
+	driver.find_element(
+		by=By.CSS_SELECTOR,
+		value="input[name=delete_btn]").click()
 	driver.switch_to.alert.accept()
+
 
 def expectInputIsEmpty(input):
 	def _predicate(driver:WebDriver):
 		target = driver.find_element(*input)
 		return target.get_property("value") == ""
-
 	return _predicate
 
 
@@ -120,7 +130,7 @@ class TestRunner(unittest.TestCase):
 			if headless:
 				options.headless = True
 			self.driver = webdriver.Firefox(options=options)
-		elif browser == "chrome" or browser == "chromium":
+		elif browser in ("chrome", "chromium"):
 			options = ChromeOptions()
 			if headless:
 				options.headless = True
@@ -137,15 +147,18 @@ class TestRunner(unittest.TestCase):
 		elem = self.driver.find_element(by=By.ID, value="board-subtitle")
 		self.assertIn("Board for testing stuff", elem.text)
 		openQR(self.driver)
-		self.assertTrue(qrIsVisible(self.driver), "Confirm that the QR box was properly opened")
+		self.assertTrue(qrIsVisible(self.driver),
+			"Confirm that the QR box was properly opened")
 		closeQR(self.driver)
-		self.assertFalse(qrIsVisible(self.driver), "Confirm that the QR box was properly closed")
+		self.assertFalse(qrIsVisible(self.driver),
+			"Confirm that the QR box was properly closed")
 
 	def test_localStorage(self):
 		gotoPage(self.driver, testingBoard)
 		localStorage = LocalStorage(self.driver)
 		localStorage["foo"] = "bar"
-		self.assertEqual(localStorage.get("foo"), "bar", "making sure that LocalStorage.get works")
+		self.assertEqual(localStorage.get("foo"), "bar",
+			"making sure that LocalStorage.get works")
 		self.assertGreater(localStorage.__len__(), 0)
 
 	def test_staffLogin(self):
@@ -163,17 +176,22 @@ class TestRunner(unittest.TestCase):
 		gotoPage(self.driver, "manage?action=boards")
 
 		# fill out the board creation form
-		self.driver.find_element(by=By.NAME, value="dir").send_keys("seleniumtesting")
-		self.driver.find_element(by=By.NAME, value="title").send_keys("Selenium testing")
-		self.driver.find_element(by=By.NAME, value="subtitle").send_keys("Board for testing Selenium")
-		self.driver.find_element(by=By.NAME, value="description").send_keys("Board for testing Selenium")
+		self.driver.find_element(by=By.NAME, value="dir").\
+			send_keys("seleniumtesting")
+		self.driver.find_element(by=By.NAME, value="title").\
+			send_keys("Selenium testing")
+		self.driver.find_element(by=By.NAME, value="subtitle").\
+			send_keys("Board for testing Selenium")
+		self.driver.find_element(by=By.NAME, value="description").\
+			send_keys("Board for testing Selenium")
 		self.driver.find_element(by=By.NAME, value="docreate").click()
 		self.driver.switch_to.alert.accept()
 		WebDriverWait(self.driver, 10).until(
-			EC.presence_of_element_located((By.CSS_SELECTOR, 'div#topbar a[href="/seleniumtesting/"]')))
+			EC.presence_of_element_located((
+				By.CSS_SELECTOR,
+				'div#topbar a[href="/seleniumtesting/"]')))
 
 		makePostOnPage("seleniumtesting", self)
-
 
 	def test_makeThread(self):
 		makePostOnPage(testingBoard, self)
@@ -191,10 +209,14 @@ class TestRunner(unittest.TestCase):
 			gotoPage(self.driver, "manage?action=boards")
 
 			# fill out the board creation form
-			self.driver.find_element(by=By.NAME, value="dir").send_keys("test2")
-			self.driver.find_element(by=By.NAME, value="title").send_keys("Testing board #2")
-			self.driver.find_element(by=By.NAME, value="subtitle").send_keys("Board for testing thread moving")
-			self.driver.find_element(by=By.NAME, value="description").send_keys("Board for testing thread moving")
+			self.driver.find_element(by=By.NAME, value="dir").\
+				send_keys("test2")
+			self.driver.find_element(by=By.NAME, value="title").\
+				send_keys("Testing board #2")
+			self.driver.find_element(by=By.NAME, value="subtitle").\
+				send_keys("Board for testing thread moving")
+			self.driver.find_element(by=By.NAME, value="description").\
+				send_keys("Board for testing thread moving")
 			self.driver.find_element(by=By.NAME, value="docreate").click()
 			self.driver.switch_to.alert.accept()
 			WebDriverWait(self.driver, 10).until(
@@ -217,27 +239,36 @@ class TestRunner(unittest.TestCase):
 
 		cur_url = self.driver.current_url
 		threadID = threadRE.findall(cur_url)[0][1]
-		self.driver.find_element(by=By.CSS_SELECTOR, value=("input#check%s"%threadID)).click()
+		self.driver.find_element(
+			by=By.CSS_SELECTOR,
+			value=("input#check%s"%threadID)).click()
 		cur_url = self.driver.current_url
-		self.driver.find_element(by=By.CSS_SELECTOR, value="input[name=move_btn]").click()
+		self.driver.find_element(
+			by=By.CSS_SELECTOR,
+			value="input[name=move_btn]").click()
 		# wait for response to move_btn
 		WebDriverWait(self.driver, 10).until(
 			EC.title_contains("Move thread #%s" % threadID))
 
-		self.driver.find_element(by=By.CSS_SELECTOR, value="input[type=submit]").click()
+		self.driver.find_element(
+			by=By.CSS_SELECTOR,
+			value="input[type=submit]").click()
 		# wait for response to move request (domove=1)
 		WebDriverWait(self.driver, 10).until(
 			EC.url_matches(threadRE))
 		
 		self.assertEqual(
-			self.driver.find_element(by=By.CSS_SELECTOR, value="h1#board-title").text,
+			self.driver.find_element(
+				by=By.CSS_SELECTOR,
+				value="h1#board-title").text,
 			"/test2/ - Testing board #2",
 			"Verify that we properly moved the thread to /test2/")
 
 		deletePost(self.driver, int(threadID), "")
 		WebDriverWait(self.driver, 10).until(
 			EC.url_changes(cur_url))
-		self.assertNotIn("Error :c", self.driver.title, "No errors when we try to delete the moved thread")
+		self.assertNotIn("Error :c", self.driver.title,
+			"No errors when we try to delete the moved thread")
 
 	def tearDown(self):
 		if not keepOpen:
@@ -269,16 +300,21 @@ def startBrowserTests(testBrowser:str, testHeadless=False, testKeepOpen=False, s
 	unittest.TextTestRunner(verbosity=3, descriptions=True).run(suite)
 
 
-def parseArgs(parser:argparse.ArgumentParser):
+def parseArgs(argParser:argparse.ArgumentParser):
 	testable_browsers = ("firefox","chrome","chromium")
 
-	parser.add_argument("--browser", choices=testable_browsers, required=True)
-	parser.add_argument("--site", default=testingSite, help=("Sets the site to be used for testing, defaults to %s" % testingSite))
-	parser.add_argument("--board", default=testingBoard, help="Sets the board to be used for testing")
-	parser.add_argument("--headless", action="store_true", help="If set, the driver will run without opening windows (overrides --keepopen if it is set)")
-	parser.add_argument("--keepopen", action="store_true", help="If set, the browser windows will not automatically close after the tests are complete")
-	parser.add_argument("--singletest", default="", help="If specified, only the test method with this name will be run")
-	return parser.parse_args()
+	argParser.add_argument("--browser", choices=testable_browsers, required=True)
+	argParser.add_argument("--site", default=testingSite,
+		help=("Sets the site to be used for testing, defaults to %s" % testingSite))
+	argParser.add_argument("--board", default=testingBoard,
+		help="Sets the board to be used for testing")
+	argParser.add_argument("--headless", action="store_true",
+		help="If set, the driver will run without opening windows (overrides --keepopen if it is set)")
+	argParser.add_argument("--keepopen", action="store_true",
+		help="If set, the browser windows will not automatically close after the tests are complete")
+	argParser.add_argument("--singletest", default="",
+		help="If specified, only the test method with this name will be run")
+	return argParser.parse_args()
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Browser testing via Selenium")
