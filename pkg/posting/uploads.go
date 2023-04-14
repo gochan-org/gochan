eef1599024ae6f142e5c523aca7736a58b858a67
Eggbertx <eggbertx@users.noreply.github.com>
2023-04-14 08:28:23 -0700

    Organize thumbnailing-specific functions into their own source file

--- pkg/posting/uploads.go
+++ pkg/posting/uploads.go
@@ -2,7 +2,6 @@ package posting
 
 import (
 	"crypto/md5"
-	"errors"
 	"fmt"
 	"html"
 	"image"
@@ -83,11 +82,16 @@ func AttachUploadFromRequest(request *http.Request, writer http.ResponseWriter, 
 	thumbPath := path.Join(documentRoot, postBoard.Dir, "thumb", upload.ThumbnailPath("thumb"))
 	catalogThumbPath := path.Join(documentRoot, postBoard.Dir, "thumb", upload.ThumbnailPath("catalog"))
 
+	boardConfig := config.GetBoardConfig(postBoard.Dir)
+	errEv.
+		Str("originalFilename", upload.OriginalFilename).
+		Str("filePath", filePath)
+	if post.ThreadID == 0 {
+		errEv.Str("catalogThumbPath", catalogThumbPath)
+	}
+
 	if err = os.WriteFile(filePath, data, config.GC_FILE_MODE); err != nil {
-		errEv.Err(err).Caller().
-			Str("filename", upload.Filename).
-			Str("originalFilename", upload.OriginalFilename).
-			Send()
+		errEv.Err(err).Caller().Send()
 		server.ServeError(writer, fmt.Sprintf("Couldn't write file %q", upload.OriginalFilename), wantsJSON, map[string]interface{}{
 			"filename":         upload.Filename,
 			"originalFilename": upload.OriginalFilename,
@@ -100,12 +104,7 @@ func AttachUploadFromRequest(request *http.Request, writer http.ResponseWriter, 
 			Str("filePath", filePath).Str("triggeredEvent", "upload-saved").
 			Msg("Recovered from a panic in event handler")
 	}
-	errEv.
-		Str("filename", handler.Filename).
-		Str("filePath", filePath).
-		Str("thumbPath", thumbPath)
 
-	boardConfig := config.GetBoardConfig(postBoard.Dir)
 	if ext == "webm" || ext == "mp4" {
 		infoEv.Str("post", "withVideo").
 			Str("filename", handler.Filename).
@@ -113,8 +112,6 @@ func AttachUploadFromRequest(request *http.Request, writer http.ResponseWriter, 
 		if post.ThreadID == 0 {
 			if err := createVideoThumbnail(filePath, thumbPath, boardConfig.ThumbWidth); err != nil {
 				errEv.Err(err).Caller().
-					Str("filePath", filePath).
-					Str("thumbPath", thumbPath).
 					Int("thumbWidth", boardConfig.ThumbWidth).
 					Msg("Error creating video thumbnail")
 				server.ServeErrorPage(writer, "Error creating video thumbnail: "+err.Error())
@@ -123,7 +120,6 @@ func AttachUploadFromRequest(request *http.Request, writer http.ResponseWriter, 
 		} else {
 			if err := createVideoThumbnail(filePath, thumbPath, boardConfig.ThumbWidthReply); err != nil {
 				errEv.Err(err).Caller().
-					Str("filePath", filePath).
 					Str("thumbPath", thumbPath).
 					Int("thumbWidth", boardConfig.ThumbWidthReply).
 					Msg("Error creating video thumbnail for reply")
@@ -134,7 +130,6 @@ func AttachUploadFromRequest(request *http.Request, writer http.ResponseWriter, 
 
 		if err := createVideoThumbnail(filePath, catalogThumbPath, boardConfig.ThumbWidthCatalog); err != nil {
 			errEv.Err(err).Caller().
-				Str("filePath", filePath).
 				Str("thumbPath", thumbPath).
 				Int("thumbWidth", boardConfig.ThumbWidthCatalog).
 				Msg("Error creating video thumbnail for catalog")
@@ -276,63 +271,6 @@ func AttachUploadFromRequest(request *http.Request, writer http.ResponseWriter, 
 	return upload, false
 }
 
-func getBoardThumbnailSize(boardDir string, thumbType string) (int, int) {
-	boardCfg := config.GetBoardConfig(boardDir)
-	switch thumbType {
-	case "op":
-		return boardCfg.ThumbWidth, boardCfg.ThumbHeight
-	case "reply":
-		return boardCfg.ThumbWidthReply, boardCfg.ThumbHeightReply
-	case "catalog":
-		return boardCfg.ThumbWidth, boardCfg.ThumbHeight
-	}
-	// todo: use reflect package to print location to error log, because this shouldn't happen
-	return -1, -1
-}
-
-func createImageThumbnail(imageObj image.Image, boardDir string, thumbType string) image.Image {
-	thumbWidth, thumbHeight := getBoardThumbnailSize(boardDir, thumbType)
-
-	oldRect := imageObj.Bounds()
-	if thumbWidth >= oldRect.Max.X && thumbHeight >= oldRect.Max.Y {
-		return imageObj
-	}
-
-	thumbW, thumbH := getThumbnailSize(oldRect.Max.X, oldRect.Max.Y, boardDir, thumbType)
-	imageObj = imaging.Resize(imageObj, thumbW, thumbH, imaging.CatmullRom) // resize to 600x400 px using CatmullRom cubic filter
-	return imageObj
-}
-
-func shouldCreateThumbnail(imgPath string, imgWidth int, imgHeight int, thumbWidth int, thumbHeight int) bool {
-	ext := strings.ToLower(path.Ext(imgPath))
-	if ext == ".gif" {
-		numFrames, err := numImageFrames(imgPath)
-		if err != nil {
-			gcutil.LogError(err).
-				Str("imgPath", imgPath).Send()
-			return true
-		}
-		if numFrames > 1 {
-			return true
-		}
-	}
-
-	return imgWidth > thumbWidth || imgHeight > thumbHeight
-}
-
-func createVideoThumbnail(video, thumb string, size int) error {
-	sizeStr := strconv.Itoa(size)
-	outputBytes, err := exec.Command("ffmpeg", "-y", "-itsoffset", "-1", "-i", video, "-vframes", "1", "-filter:v", "scale='min("+sizeStr+"\\, "+sizeStr+"):-1'", thumb).CombinedOutput()
-	if err != nil {
-		outputStringArr := strings.Split(string(outputBytes), "\n")
-		if len(outputStringArr) > 1 {
-			outputString := outputStringArr[len(outputStringArr)-2]
-			err = errors.New(outputString)
-		}
-	}
-	return err
-}
-
 func getVideoInfo(path string) (map[string]int, error) {
 	vidInfo := make(map[string]int)
 
@@ -358,28 +296,6 @@ func getNewFilename() string {
 	now := time.Now().Unix()
 	rand.Seed(now)
 	return strconv.Itoa(int(now)) + strconv.Itoa(rand.Intn(98)+1)
-}
-
-// find out what out thumbnail's width and height should be, partially ripped from Kusaba X
-func getThumbnailSize(uploadWidth, uploadHeight int, boardDir string, thumbType string) (newWidth, newHeight int) {
-	thumbWidth, thumbHeight := getBoardThumbnailSize(boardDir, thumbType)
-	if uploadWidth < thumbWidth && uploadHeight < thumbHeight {
-		newWidth = uploadWidth
-		newHeight = uploadHeight
-	} else if uploadWidth == uploadHeight {
-		newWidth = thumbWidth
-		newHeight = thumbHeight
-	} else {
-		var percent float32
-		if uploadWidth > uploadHeight {
-			percent = float32(thumbWidth) / float32(uploadWidth)
-		} else {
-			percent = float32(thumbHeight) / float32(uploadHeight)
-		}
-		newWidth = int(float32(uploadWidth) * percent)
-		newHeight = int(float32(uploadHeight) * percent)
-	}
-	return
 }
 
 func numImageFrames(imgPath string) (int, error) {
