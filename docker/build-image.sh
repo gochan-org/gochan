608ff24d9a460114380f9fcc00beacdbb0c8ab33
Eggbertx <eggbertx@users.noreply.github.com>
2023-04-23 20:52:41 -0700

    Move chain command in Dockerfile CMD to startup script, update Go version in Docker

---
+++ docker/build-image.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -euo pipefail
+
+apk --no-cache add \
+	postgresql-client	\
+	mariadb-client		\
+	nginx 				\
+	ffmpeg				\
+	python3				\
+	git					\
+	gcc					\
+	musl-dev			\
+	openssl				\
+	bash
+
+mkdir -p /root/bin
+
+ln -s /usr/lib/go-1.18/bin/* /root/bin/
+export PATH=$PATH:/root/bin
+echo "export PATH=$PATH:/root/bin" >> /root/.bashrc
+rm -f /etc/nginx/sites-enabled/* /etc/nginx/sites-available/*
+mkdir -p /var/lib/nginx
+mkdir -p /var/lib/nginx/tmp
+mkdir -p /run/nginx/
+
+rm -f /etc/nginx/http.d/default.conf
+
+
+# The openssl command will generate self-signed certificate since some browsers like
+# Firefox and Chrome automatically do HTTPS requests. this will likely show a warning in
+# the browser, which you can ignore
+mkdir -p /etc/ssl/private
+openssl req -x509 -nodes -days 7305 -newkey rsa:2048 \
+	-keyout /etc/ssl/private/nginx-selfsigned.key \
+	-out /etc/ssl/certs/nginx-selfsigned.crt \
+	-subj "/CN=192.168.56.3"
