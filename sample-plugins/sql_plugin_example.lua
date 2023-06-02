ee8af4a2bd06cf0780089adafd468e09febe7bbb
Eggbertx <eggbertx@users.noreply.github.com>
2023-06-02 13:45:17 -0700

    Use boards instead of staff table for sql plugin example

--- sample-plugins/sql_plugin_example.lua
+++ sample-plugins/sql_plugin_example.lua
@@ -1,12 +1,12 @@
 event_register({"db-initialized"}, function(tr)
 	print("Testing SELECT query from Lua plugin")
-	rows, err = db_query("SELECT id, username FROM DBPREFIXstaff where id = ?", {1})
+	rows, err = db_query("SELECT id, dir, title FROM DBPREFIXboards where id > ?", {1})
 	if(err ~= nil) then
 		print(err.Error(err))
 		return
 	end
 
-	print("rows.Next():")
+	print("Boards (id > 1):")
 	while rows.Next(rows) do
 		rows_table = {}
 		err = db_scan_rows(rows, rows_table)
@@ -14,8 +14,8 @@ event_register({"db-initialized"}, function(tr)
 			print(err.Error(err))
 			return
 		end
-		print(string.format("rows_table.id: %#v, rorws_table.username: %#v", rows_table.id, rows_table.username))
+		print(string.format("rows_table.id: %#v, rows_table.dir: %#v, rows_table.title = %#v",
+			rows_table.id, rows_table.dir, rows_table.title))
 	end
 	rows.Close(rows)
-	print("Done")
 end)
