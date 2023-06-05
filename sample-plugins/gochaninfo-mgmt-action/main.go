599e92bdf452c8956e2f3738753286b0769a166e
Eggbertx <eggbertx@users.noreply.github.com>
2023-06-05 13:25:54 -0700

    Add a native sample plugin for showing Gochan info to the admin

---
+++ sample-plugins/gochaninfo-mgmt-action/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+	"strings"
+
+	"github.com/gochan-org/gochan/pkg/config"
+	"github.com/gochan-org/gochan/pkg/gcsql"
+	"github.com/gochan-org/gochan/pkg/manage"
+	"github.com/rs/zerolog"
+)
+
+const (
+	infoPage = `<b>Gochan version:</b> %s<br/>
+<b>Go version:</b> %s<br/>
+<b>GOOS:</b> %s<br/>
+<b>DB type:</b> %s<br/>
+<b>Loaded plugins:</b><br/>
+<ul><li>%s</li></ul>`
+)
+
+func InitPlugin() error {
+	var err error
+	manage.RegisterManagePage("gochaninfo", "Gochan info", manage.AdminPerms, manage.NoJSON,
+		func(writer http.ResponseWriter, request *http.Request, staff *gcsql.Staff, wantsJSON bool, infoEv, errEv *zerolog.Event) (output interface{}, err error) {
+			return fmt.Sprintf(infoPage,
+					config.GetVersion().String(),
+					runtime.Version(),
+					runtime.GOOS,
+					config.GetSystemCriticalConfig().DBtype,
+					strings.Join(config.GetSystemCriticalConfig().Plugins, "</li><li>")),
+				nil
+		},
+	)
+	return err
+}
