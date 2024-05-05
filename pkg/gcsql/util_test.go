caea5ee77e281e4e83ac82b34b2a0ba7f0ccf656
Eggbertx <eggbertx@users.noreply.github.com>
2024-05-04 18:22:30 -0700

    Add tests for QueryRowContextSQL

--- pkg/gcsql/util_test.go
+++ pkg/gcsql/util_test.go
@@ -3,6 +3,7 @@ package gcsql
 import (
 	"context"
 	"database/sql"
+	"database/sql/driver"
 	"testing"
 	"time"
 
@@ -11,9 +12,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type testCasePrepareContextSQL struct {
+type testCase[T any] struct {
 	name       string
-	f          func(context.Context, string, *sql.Tx) (*sql.Stmt, error)
+	f          T
 	shouldFail bool
 	timeout    time.Duration
 	delay      time.Duration
@@ -33,7 +34,9 @@ func setupMockDB(t *testing.T) sqlmock.Sqlmock {
 	return mock
 }
 
-func tcPrepareContextSQL(t *testing.T, mock sqlmock.Sqlmock, tC *testCasePrepareContextSQL) {
+type prepareFunc func(context.Context, string, *sql.Tx) (*sql.Stmt, error)
+
+func tcPrepareContextSQL(t *testing.T, mock sqlmock.Sqlmock, tC *testCase[prepareFunc]) {
 	const query = "SELECT * FROM DBPREFIXposts"
 	const expectQuery = `SELECT \* FROM posts`
 
@@ -52,7 +55,7 @@ func tcPrepareContextSQL(t *testing.T, mock sqlmock.Sqlmock, tC *testCasePrepare
 		return
 	}
 
-	stmt, err := PrepareContextSQL(ctx, query, tx)
+	stmt, err := tC.f(ctx, query, tx)
 	if tC.shouldFail {
 		assert.Error(t, err)
 		return
@@ -78,7 +81,7 @@ func TestPrepareContextSQL(t *testing.T) {
 	}
 
 	mock := setupMockDB(t)
-	testCases := []testCasePrepareContextSQL{
+	testCases := []testCase[prepareFunc]{
 		{
 			name: "func",
 			f:    PrepareContextSQL,
@@ -104,6 +107,162 @@ func TestPrepareContextSQL(t *testing.T) {
 	for c, tC := range testCases {
 		t.Run(tC.name, func(t *testing.T) {
 			tcPrepareContextSQL(t, mock, &testCases[c])
+		})
+	}
+}
+
+type execFunc func(context.Context, *sql.Tx, string, ...any) (sql.Result, error)
+
+func tcExecContextSQL(t *testing.T, mock sqlmock.Sqlmock, tC *testCase[execFunc]) {
+	const query = "INSERT INTO DBPREFIXposts (name) VALUES(?)"
+	const expectQuery = `INSERT INTO posts \(name\) VALUES\(\?\)`
+
+	var ctx context.Context
+	if tC.timeout > 0 {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(context.Background(), tC.timeout)
+		defer cancel()
+	} else {
+		ctx = context.Background()
+	}
+	mock.ExpectBegin()
+	mock.ExpectPrepare(expectQuery).
+		WillDelayFor(tC.delay).
+		ExpectExec().WithArgs("Test").
+		WillReturnResult(driver.ResultNoRows)
+
+	tx, err := BeginContextTx(ctx)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	_, err = tC.f(ctx, tx, query, "Test")
+	if tC.shouldFail {
+		assert.Error(t, err)
+		return
+	}
+	if !assert.NoError(t, err) {
+		return
+	}
+	if !assert.NoError(t, mock.ExpectationsWereMet()) {
+		return
+	}
+	mock.ExpectCommit()
+	assert.NoError(t, tx.Commit())
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestExecContextSQL(t *testing.T) {
+	_, err := ExecContextSQL(nil, nil, "")
+	if !assert.Error(t, err) {
+		return
+	}
+
+	mock := setupMockDB(t)
+	testCases := []testCase[execFunc]{
+		{
+			name: "func",
+			f:    ExecContextSQL,
+		},
+		{
+			name: "gcdb method",
+			f:    gcdb.ExecContextSQL,
+		},
+		{
+			name:    "delay (no fail)",
+			f:       ExecContextSQL,
+			delay:   time.Second,
+			timeout: 2 * time.Second,
+		},
+		{
+			name:       "delay (times out)",
+			f:          ExecContextSQL,
+			delay:      2 * time.Second,
+			timeout:    time.Second,
+			shouldFail: true,
+		},
+	}
+	for c, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			tcExecContextSQL(t, mock, &testCases[c])
+		})
+	}
+}
+
+type funcQueryRowContextSQL func(context.Context, *sql.Tx, string, []any, []any) error
+
+func tcQueryRowContextSQL(t *testing.T, mock sqlmock.Sqlmock, tC *testCase[funcQueryRowContextSQL]) {
+	const query = "SELECT NAME FROM DBPREFIXposts WHERE id = ?"
+	const expectQuery = `SELECT NAME FROM posts WHERE id = \?`
+
+	var ctx context.Context
+	if tC.timeout > 0 {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(context.Background(), tC.timeout)
+		defer cancel()
+	} else {
+		ctx = context.Background()
+	}
+	mock.ExpectBegin()
+	mock.ExpectPrepare(expectQuery).
+		ExpectQuery().WithArgs(1).
+		WillDelayFor(tC.delay).WillReturnRows(sqlmock.NewRows([]string{"name"}).AddRow("Name"))
+
+	tx, err := BeginContextTx(ctx)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	var out string
+	err = tC.f(ctx, tx, query, []any{1}, []any{&out})
+	if tC.shouldFail {
+		assert.Error(t, err)
+		return
+	}
+	if !assert.NoError(t, err) {
+		return
+	}
+	if !assert.NoError(t, mock.ExpectationsWereMet()) {
+		return
+	}
+	mock.ExpectCommit()
+	assert.NoError(t, tx.Commit())
+	assert.NoError(t, mock.ExpectationsWereMet())
+}
+
+func TestQueryRowContextSQL(t *testing.T) {
+	err := QueryRowContextSQL(nil, nil, "", nil, nil)
+	if !assert.Error(t, err) {
+		return
+	}
+
+	mock := setupMockDB(t)
+	testCases := []testCase[funcQueryRowContextSQL]{
+		{
+			name: "func",
+			f:    QueryRowContextSQL,
+		},
+		{
+			name: "gcdb method",
+			f:    gcdb.QueryRowContextSQL,
+		},
+		{
+			name:    "delay (no fail)",
+			f:       QueryRowContextSQL,
+			delay:   time.Second,
+			timeout: 2 * time.Second,
+		},
+		{
+			name:       "delay (times out)",
+			f:          QueryRowContextSQL,
+			delay:      2 * time.Second,
+			timeout:    time.Second,
+			shouldFail: true,
+		},
+	}
+	for c, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			tcQueryRowContextSQL(t, mock, &testCases[c])
 		})
 	}
 }
