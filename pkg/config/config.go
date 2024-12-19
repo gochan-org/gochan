358a84c7e30ff7fe6e717055c1330baefa51f0f3
onihilist <mashirooo.yt@gmail.com>
2024-12-19 15:23:16 +0100

    Small fix config.go

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -39,8 +39,8 @@ type GochanConfig struct {
 }
 
 func SetMockConfig() {
-  
-	cfg = &GochanConfig{
+
+	Cfg = &GochanConfig{
 
 		SystemCriticalConfig: SystemCriticalConfig{
 			ListenIP:     "127.0.0.1",
