b95ae1eb1546c981ff5041c02457e087bd06a4b3
Eggbertx <eggbertx@users.noreply.github.com>
2025-08-03 12:57:05 -0700

    Download previous version only if it doesn't already exist, and properly reset the previous version's testing database

--- tools/get_previous_version.sh
+++ tools/get_previous_version.sh
@@ -16,49 +16,59 @@ if [ "$USER" != "vagrant" ]; then
 fi
 
 cd ~
-rm -f $RELEASE_GZ
-echo "Downloading $RELEASE_GZ"
-wget -q --show-progress $RELEASE_URL
+if [[ ! -e "$RELEASE_GZ" ]]; then
+	echo "Downloading $RELEASE_URL"
+	wget -q --show-progress $RELEASE_URL
+fi
+rm -rf $RELEASE_DIR
 echo "Extracting $RELEASE_GZ"
 tar -xf gochan-${TESTING_VERSION}_linux.tar.gz
 cd $RELEASE_DIR
 
-cp examples/configs/gochan.example.json gochan.json
-echo "Modifying $PWD/gochan.json for testing migration"
-sed -i gochan.json \
-	-e 's/"Port": .*/"Port": 9000,/' \
-	-e 's/"UseFastCGI": false/"UseFastCGI": true/' \
-	-e "s/\"DBtype\": .*/\"DBtype\": \""$DBTYPE"\",/" \
-	-e 's/"DBpassword": ""/"DBpassword": "gochan"/' \
-	-e 's/"LogDir": .*/"LogDir": "log",/' \
-	-e 's/"TemplateDir": .*/"TemplateDir": "templates",/' \
-	-e 's/"DocumentRoot": .*/"DocumentRoot": "html",/' \
-	-e 's/"DBname": "gochan"/"DBname": "gochan_prev"/' \
-	-e 's/"DBprefix": .*/"DBprefix": "",/' \
-	-e 's/"SiteName": "Gochan"/"SiteName": "Gochan Migration Test"/' \
-	-e 's/"SiteSlogan": ""/"SiteSlogan": "Gochan instance used for testing gochan-migrate -updatedb"/' \
-	-e 's/"DebugMode": false/"DebugMode": true/' \
-	-e 's/"Verbosity": 0/"Verbosity": 1/' \
-	-e 's/"GeoIPType": .*/"GeoIPType": "",/' \
-	-e 's/"EnableGeoIP": .*/"EnableGeoIP": false,/'
+if [[ ! -f gochan.json ]]; then
+	cp -f examples/configs/gochan.example.json gochan.json
+	echo "Modifying $PWD/gochan.json for testing migration"
+	sed -i gochan.json \
+		-e 's/"Port": .*/"Port": 9000,/' \
+		-e 's/"UseFastCGI": false/"UseFastCGI": true/' \
+		-e "s/\"DBtype\": .*/\"DBtype\": \""$DBTYPE"\",/" \
+		-e 's/"DBpassword": ""/"DBpassword": "gochan"/' \
+		-e 's/"LogDir": .*/"LogDir": "log",/' \
+		-e 's/"TemplateDir": .*/"TemplateDir": "templates",/' \
+		-e 's/"DocumentRoot": .*/"DocumentRoot": "html",/' \
+		-e 's/"DBname": "gochan"/"DBname": "gochan_prev"/' \
+		-e 's/"DBprefix": .*/"DBprefix": "",/' \
+		-e 's/"SiteName": "Gochan"/"SiteName": "Gochan Migration Test"/' \
+		-e 's/"SiteSlogan": ""/"SiteSlogan": "Gochan instance used for testing gochan-migrate -updatedb"/' \
+		-e 's/"DebugMode": false/"DebugMode": true/' \
+		-e 's/"Verbosity": 0/"Verbosity": 1/' \
+		-e 's/"GeoIPType": .*/"GeoIPType": "",/' \
+		-e 's/"EnableGeoIP": .*/"EnableGeoIP": false,/'
+fi
 
 if [ "$DBTYPE" = "mysql" ]; then
 	echo "Creating testing MySQL DB 'gochan_prev' if it doesn't already exist"
-	sudo mysql <<- EOF1
+	sudo mysql <<- EOF
+	DROP DATABASE IF EXISTS gochan_prev;
 	CREATE DATABASE IF NOT EXISTS gochan_prev;
 	GRANT USAGE ON *.* TO gochan IDENTIFIED BY 'gochan'; \
 	GRANT ALL PRIVILEGES ON gochan_prev.* TO gochan; \
 	SET PASSWORD FOR 'gochan'@'%' = PASSWORD('gochan'); \
 	FLUSH PRIVILEGES;
-	EOF1
+	EOF
 elif [ "$DBTYPE" = "postgresql" ]; then
 	echo "Creating testing PostgreSQL DB 'gochan_prev' if it doesn't already exist"
 	sed -i gochan.json \
 		-e 's/"DBhost": ".*"/"DBhost": "127.0.0.1"/'
 	sudo -u postgres psql -f - <<- EOF1
+	DROP DATABASE IF EXISTS gochan_prev;
 	CREATE DATABASE gochan_prev;
 	GRANT ALL PRIVILEGES ON DATABASE gochan_prev TO gochan;
 	EOF1
+elif [ "$DBTYPE" = "sqlite3" ]; then
+	sed -i gochan.json \
+		-e 's/"DBhost": ".*"/"DBhost": "gochan.db"/'
+	rm -f gochan.db
 else
 	echo "Currently using unsupported \$DBTYPE: $DBTYPE"
 	exit 1
