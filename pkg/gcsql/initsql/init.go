87624b496bee0e54a15f45cc20e3d9efc3d6021c
Eggbertx <eggbertx@users.noreply.github.com>
2025-11-21 15:06:05 -0800

    Add IP conversion functions and comparison helper function

--- pkg/gcsql/initsql/init.go
+++ pkg/gcsql/initsql/init.go
@@ -1,8 +1,11 @@
 package initsql
 
 import (
+	"database/sql"
 	"errors"
+	"net"
 	"net/http"
+	"net/netip"
 	"path"
 	"strconv"
 	"text/template"
@@ -13,6 +16,7 @@ import (
 	"github.com/gochan-org/gochan/pkg/gctemplates"
 	"github.com/gochan-org/gochan/pkg/gcutil"
 	"github.com/gochan-org/gochan/pkg/posting/uploads"
+	"github.com/mattn/go-sqlite3"
 )
 
 func banMaskTmplFunc(ban gcsql.IPBan) string {
@@ -108,6 +112,109 @@ func sectionBoardsTmplFunc(sectionID int) []gcsql.Board {
 }
 
 func init() {
+	sql.Register("sqlite3-inet6", &sqlite3.SQLiteDriver{
+		ConnectHook: func(sc *sqlite3.SQLiteConn) error {
+			sc.RegisterFunc("inet6_aton", func(a string) []byte {
+				ip := net.ParseIP(a)
+				if ip == nil {
+					return nil
+				}
+				ip = ip.To16()
+				return ip
+			}, true)
+
+			sc.RegisterFunc("inet6_ntoa", func(n any) any {
+				var ip net.IP
+				switch v := n.(type) {
+				case []byte:
+					ip = net.IP(v).To16()
+				case int64:
+					ip = net.IPv4(byte(v>>24), byte(v>>16), byte(v>>8), byte(v)).To16()
+				}
+
+				if ip == nil {
+					return nil
+				}
+				return ip.String()
+			}, true)
+
+			sc.RegisterFunc("inet_aton", func(a string) []byte {
+				ip := net.ParseIP(a)
+				if ip == nil {
+					return nil
+				}
+				ip = ip.To16()
+				if ip.To4() == nil {
+					return nil // not a IPv4 address
+				}
+				return ip
+			}, true)
+
+			sc.RegisterFunc("inet_ntoa", func(n any) any {
+				var ip net.IP
+				switch i := n.(type) {
+				case int64:
+					ip = net.IPv4(byte(i>>24), byte(i>>16), byte(i>>8), byte(i))
+				case []byte:
+					ip = net.IP(i).To4()
+				}
+				if ip == nil {
+					return nil
+				}
+				return ip.String()
+			}, true)
+
+			sc.RegisterFunc("ip_cmp", func(ip1 any, ip2 any) any {
+				var netIP1, netIP2 net.IP
+				var netIPAddr1, netIPAddr2 netip.Addr
+				switch v := ip1.(type) {
+				case []byte:
+					netIP1 = net.IP(v)
+				case int64:
+					netIP1 = net.IPv4(byte(v>>24), byte(v>>16), byte(v>>8), byte(v)).To16()
+				case string:
+					netIP1 = net.ParseIP(v)
+					if netIP1 != nil {
+						netIP1 = netIP1.To16()
+					}
+				default:
+					return nil
+				}
+				if netIP1.To4() != nil {
+					netIP1 = netIP1.To4()
+				}
+				switch v := ip2.(type) {
+				case []byte:
+					netIP2 = net.IP(v).To16()
+				case int64:
+					netIP2 = net.IPv4(byte(v>>24), byte(v>>16), byte(v>>8), byte(v)).To16()
+				case string:
+					netIP2 = net.ParseIP(v)
+					if netIP2 != nil {
+						netIP2 = netIP2.To16()
+					}
+				default:
+					return nil
+				}
+				if netIP2.To4() != nil {
+					netIP2 = netIP2.To4()
+				}
+				if netIP1 == nil || netIP2 == nil {
+					return nil // one or both are invalid
+				}
+				if len(netIP1) != len(netIP2) {
+					return nil // can't compare different IP classes
+				}
+
+				netIPAddr1, _ = netip.AddrFromSlice(netIP1)
+				netIPAddr2, _ = netip.AddrFromSlice(netIP2)
+				return netIPAddr1.Compare(netIPAddr2)
+			}, true)
+
+			return nil
+		},
+	})
+
 	events.RegisterEvent([]string{"reset-boards-sections"}, func(_ string, _ ...any) error {
 		if config.GetSQLConfig().DBhost != "" {
 			// Only reset if SQL is configured
--- pkg/gcsql/initsql/init_test.go
+++ pkg/gcsql/initsql/init_test.go
@@ -1,20 +1,31 @@
 package initsql
 
 import (
+	"database/sql"
+	"net"
 	"testing"
 
 	"github.com/gochan-org/gochan/pkg/gcsql"
 	"github.com/stretchr/testify/assert"
 )
 
+type banMaskTestCase struct {
+	desc       string
+	rangeStart string
+	rangeEnd   string
+	expects    string
+	banID      int
+}
+
+type inetConversionTestCase[I any, E any] struct {
+	desc         string
+	input        I
+	inetExpects  E
+	inet6Expects E
+}
+
 func TestBanMaskTmplFunc(t *testing.T) {
-	testCases := []struct {
-		desc       string
-		rangeStart string
-		rangeEnd   string
-		expects    string
-		banID      int
-	}{
+	testCases := []banMaskTestCase{
 		{
 			desc: "expect empty string if either value is enpty",
 		},
@@ -65,4 +76,134 @@ func TestBanMaskTmplFunc(t *testing.T) {
 			assert.Equal(tr, tC.expects, result)
 		})
 	}
+}
+
+func TestInetNtoA(t *testing.T) {
+	testCases := []inetConversionTestCase[any, sql.NullString]{
+		{
+			desc:         "convert valid IP number to IPv4 address",
+			input:        3232249859,
+			inet6Expects: sql.NullString{String: "192.168.56.3", Valid: true},
+			inetExpects:  sql.NullString{String: "192.168.56.3", Valid: true},
+		},
+		{
+			desc:         "convert valid IPv6 address bytes to string",
+			input:        net.ParseIP("2601::1"),
+			inet6Expects: sql.NullString{String: "2601::1", Valid: true},
+		},
+		{
+			desc:  "convert invalid IP address bytes to string",
+			input: []byte{1, 2, 3},
+		},
+	}
+	db, err := sql.Open("sqlite3-inet6", ":memory:")
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+	defer db.Close()
+	var ipStr sql.NullString
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			if !assert.NoError(t, db.QueryRow("SELECT INET_NTOA(?)", tC.input).Scan(&ipStr)) {
+				t.FailNow()
+			}
+			assert.Equal(t, tC.inetExpects, ipStr)
+			if !assert.NoError(t, db.QueryRow("SELECT INET6_NTOA(?)", tC.input).Scan(&ipStr)) {
+				t.FailNow()
+			}
+			assert.Equal(t, tC.inet6Expects, ipStr)
+		})
+	}
+	assert.NoError(t, db.Close())
+}
+
+func TestInetAtoN(t *testing.T) {
+	testCases := []inetConversionTestCase[string, []byte]{
+		{
+			desc:         "convert valid IPv4 address string to bytes",
+			input:        "192.168.56.3",
+			inetExpects:  net.ParseIP("192.168.56.3").To16(),
+			inet6Expects: net.ParseIP("192.168.56.3").To16(),
+		},
+		{
+			desc:         "convert valid IPv6 address string to bytes",
+			input:        "2601::1",
+			inetExpects:  nil,
+			inet6Expects: net.ParseIP("2601::1").To16(),
+		},
+		{
+			desc:         "invalid IP address returns null",
+			input:        "hmm",
+			inetExpects:  nil,
+			inet6Expects: nil,
+		},
+	}
+	db, err := sql.Open("sqlite3-inet6", ":memory:")
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+	defer db.Close()
+	var ip []byte
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			if !assert.NoError(t, db.QueryRow("SELECT INET_ATON(?)", tC.input).Scan(&ip)) {
+				t.FailNow()
+			}
+			assert.Equal(t, tC.inetExpects, ip)
+			if !assert.NoError(t, db.QueryRow("SELECT INET6_ATON(?)", tC.input).Scan(&ip)) {
+				t.FailNow()
+			}
+			assert.Equal(t, tC.inet6Expects, ip)
+		})
+	}
+	assert.NoError(t, db.Close())
+}
+
+func TestIPCmp(t *testing.T) {
+	db, err := sql.Open("sqlite3-inet6", ":memory:")
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+	defer db.Close()
+
+	setup := []string{
+		"create table addrs(id integer primary key autoincrement, ip varbinary(16))",
+		"insert into addrs(ip) values (inet_aton('192.168.56.1')), (inet6_aton('192.168.56.2')), (inet6_aton('2601:8000::1')), (inet6_aton('2601:f000::'))",
+	}
+	for _, stmt := range setup {
+		_, err := db.Exec(stmt)
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
+	}
+	var result []net.IP
+	rows, err := db.Query("SELECT ip FROM addrs WHERE ip_cmp(ip, '192.168.56.1') > 0")
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+	for rows.Next() {
+		var ip net.IP
+		if !assert.NoError(t, rows.Scan(&ip)) {
+			t.FailNow()
+		}
+		result = append(result, ip)
+		t.Log(ip.String())
+	}
+	assert.Equal(t, 1, len(result))
+	result = []net.IP{}
+	rows, err = db.Query("SELECT ip FROM addrs WHERE ip_cmp(ip, '2601:9000::1') >= 0")
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+	for rows.Next() {
+		var ip net.IP
+		if !assert.NoError(t, rows.Scan(&ip)) {
+			t.FailNow()
+		}
+		result = append(result, ip)
+		// t.Log(ip)
+	}
+
+	assert.Equal(t, 1, len(result))
+	assert.NoError(t, db.Close())
 }
