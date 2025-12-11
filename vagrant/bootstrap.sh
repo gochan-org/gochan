f27a8c2a3bfbb3b57d36946b8f86929c26bcda86
Eggbertx <eggbertx@users.noreply.github.com>
2025-12-11 14:38:53 -0800

    Add option for initial configuration in Vagrantfile and bootstrap script

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -103,27 +103,29 @@ systemctl enable nginx
 systemctl restart nginx &
 wait
 
-# mkdir -p /etc/gochan
-# cp /vagrant/examples/configs/gochan.example.json /vagrant/gochan.json
-# ln -s /vagrant/gochan.json /etc/gochan/gochan.json
-# sed -i /vagrant/gochan.json \
-# 	-e 's/"Port": 8080/"Port": 9000/' \
-# 	-e 's/"UseFastCGI": false/"UseFastCGI": true/' \
-# 	-e 's#"DocumentRoot": "html"#"DocumentRoot": "/srv/gochan"#' \
-# 	-e 's#"TemplateDir": "templates"#"TemplateDir": "/usr/share/gochan/templates"#' \
-# 	-e 's#"LogDir": "log"#"LogDir": "/var/log/gochan"#' \
-# 	-e "s/\"DBtype\": .*/\"DBtype\": \"$DBTYPE\",/" \
-# 	-e 's/"SiteHost": .*/"SiteHost": "192.168.56.3",/' \
-# 	-e 's/"DBpassword": .*/"DBpassword": "gochan",/' \
-# 	-e 's/"Verbosity": 0/"Verbosity": 1/'
+if [ -n "$INITCONFIG" ]; then
+	mkdir -p /etc/gochan
+	cp /vagrant/examples/configs/gochan.example.json /vagrant/gochan.json
+	ln -s /vagrant/gochan.json /etc/gochan/gochan.json
+	sed -i /vagrant/gochan.json \
+		-e 's/"Port": 8080/"Port": 9000/' \
+		-e 's/"UseFastCGI": false/"UseFastCGI": true/' \
+		-e 's#"DocumentRoot": "html"#"DocumentRoot": "/srv/gochan"#' \
+		-e 's#"TemplateDir": "templates"#"TemplateDir": "/usr/share/gochan/templates"#' \
+		-e 's#"LogDir": "log"#"LogDir": "/var/log/gochan"#' \
+		-e "s/\"DBtype\": .*/\"DBtype\": \"$DBTYPE\",/" \
+		-e 's/"SiteHost": .*/"SiteHost": "192.168.56.3",/' \
+		-e 's/"DBpassword": .*/"DBpassword": "gochan",/' \
+		-e 's/"Verbosity": 0/"Verbosity": 1/'
 
-# if [ "$DBTYPE" = "postgresql" ]; then
-# 	sed -i /etc/gochan/gochan.json \
-# 		-e 's/"DBhost": ".*"/"DBhost": "127.0.0.1"/'
-# elif [ "$DBTYPE" = "sqlite3" ]; then
-# 	sed -i /etc/gochan/gochan.json \
-# 		-e 's#"DBhost": ".*"#"DBhost": "/etc/gochan/gochan.db"#'
-# fi
+	if [ "$DBTYPE" = "postgresql" ]; then
+		sed -i /etc/gochan/gochan.json \
+			-e 's/"DBhost": ".*"/"DBhost": "127.0.0.1"/'
+	elif [ "$DBTYPE" = "sqlite3" ]; then
+		sed -i /etc/gochan/gochan.json \
+			-e 's#"DBhost": ".*"#"DBhost": "/etc/gochan/gochan.db"#'
+	fi
+fi
 
 # a convenient script for connecting to the db, whichever type we're using
 ln -s {/vagrant/tools,/home/vagrant}/dbconnect.sh
